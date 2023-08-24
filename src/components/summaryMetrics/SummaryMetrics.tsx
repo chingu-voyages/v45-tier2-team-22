61f38e9e56366d7e9a9fdacdb41c09e553599c6b
Sensei_P <padraigo.donoghue@gmail.comgit config --global user.name Sensei_Pgit config --global user.email padraigo.donoghue@gmail.com>
2023-08-24 23:37:05 +0100

    Rollback attempt 2

--- src/components/summaryMetrics/SummaryMetrics.tsx
+++ src/components/summaryMetrics/SummaryMetrics.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./SummaryMetrics.scss";
 const SummaryMetrics = () => {
-  return <div>SummaryMetrics</div>;
+  return (
+    <div>SummaryMetrics</div>
+    )
 };
 
 export default SummaryMetrics;
