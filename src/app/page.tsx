6735db39e910374a2191284792bd9e789bae1994
Sensei_P <padraigo.donoghue@gmail.comgit config --global user.name Sensei_Pgit config --global user.email padraigo.donoghue@gmail.com>
2023-08-25 14:28:10 +0100

    Manually reverting footer back to before Footer merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import DetailData from '@/components/detailData/DetailData';
