be4f372622d4cba613dfcd0049356742272d75a0
simonC137 <simon7summers@gmail.com>
2023-08-28 22:33:24 +0100

    refactor/revert to react-data-table with styled-components

--- src/components/detailData/DetailData.tsx
+++ src/components/detailData/DetailData.tsx
@@ -28,29 +28,7 @@ const DetailData = () => {
 
 const {state, filteredData} = useAppContext();
 
-const pageNumber = 0;
-const pageSize = 10;
-const [page, setPage] = useState( () => pageNumber )
-
 const mockSearchResult = state;
-
-let searchResult = mockSearchResult.map(({geolocation, fall, reclat, reclong, id, nametype, ...rest}) => {
-  return rest;
-})
-
-for (const item of searchResult) {
-  if (item.year) {
-    item.year = item.year.slice(0, 4);
-  }
-}
-
-const handlePrevious = () => {
-  setPage(page => page - 1)
-}
-
-const handleNext = () => {
-  setPage(page => page + 1)
-}
 
 const columns = [
   {
@@ -84,86 +62,41 @@ const columns = [
 ];
 
 
-// const paginationComponentOptions = {
-//   noRowsPerPage: true
-// }
-// const customStyles = {
-//   rows: {
-//       style: {
-//           minHeight: '30px', 
-//       },
-//   },
-//   headCells: {
-//       style: {
-//           paddingLeft: '8px', 
-//           paddingRight: '8px',
-//           fontSize: '15px',
-//       },
-//   },
-//   cells: {
-//       style: {
-//           paddingLeft: '8px', 
-//           paddingRight: '8px',
-//       },
-//   },
-// };
+const paginationComponentOptions = {
+  noRowsPerPage: true
+}
+const customStyles = {
+  rows: {
+      style: {
+          minHeight: '30px', 
+      },
+  },
+  headCells: {
+      style: {
+          paddingLeft: '8px', 
+          paddingRight: '8px',
+          fontSize: '15px',
+      },
+  },
+  cells: {
+      style: {
+          paddingLeft: '8px', 
+          paddingRight: '8px',
+      },
+  },
+};
 
   return (
     <>
-      {/* <div>DetailData</div> */}
+      <div>DetailData</div>
       <div className='container'>
-        {searchResult.length > 0  && (
-          <div>
-          <table className='dataTable'>
-            <thead className='tHead'>
-              <tr className='tableHeader'>
-                {columns.map((item, index)=> (
-                  <th 
-                  className='columnHeading'
-                  key={index}>
-                    {item.name}
-                  </th>
-                )
-                )}
-              </tr>
-            </thead>
-            <tbody>
-              {Object.values(searchResult.slice(pageSize * pageNumber, (pageSize * pageNumber) + pageSize)).map((item, index) => (
-                <tr key={index} className='tableRow'>
-                  {Object.values(item).map((value, token) => (
-                    <td key={token} className='tableRow'>
-                      {value}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className='pagination'>
-                <button id='btn-previous' onClick={handlePrevious}>Previous</button>
-               
-                <button className='num-buttons' id='active'>{pageNumber + 1}</button>
-               
-                <button id='btn-next' onClick={handleNext}>Next</button>
-              </div>
-              </div>
-        )}
-
-
-
-
-
-
-
-
-
-        {/* <DataTable
+        <DataTable
           columns={columns}
           data={mockSearchResult}
           pagination
           paginationComponentOptions={paginationComponentOptions}
           customStyles={customStyles}
-        /> */}
+        />
       </div> 
     </>
   )
